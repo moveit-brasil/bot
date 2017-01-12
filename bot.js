084ed127bbc510e8b47745230a7626c3519e5686
Alessandro <alessandro_laurindo@yahoo.com.br>
2017-01-12 19:27:12 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2691,7 +2691,7 @@
 
 			kickCommand: {
 				command: 'kick',
-				rank: 'bouncer',
+				rank: 'mod',
 				type: 'startsWith',
 				functionality: function (chat, cmd) {
 					if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
