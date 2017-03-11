f5f9935f3b8188e5c894183a99e7a0bc7bff47f9
Alessandro <alessandro_laurindo@yahoo.com.br>
2017-03-11 15:06:25 -0300

    Update entrada.js

--- entrada.js
+++ entrada.js
@@ -5,9 +5,6 @@ API.on(API.USER_JOIN, user => {
 	if (user.id == "5401598")
 		API.sendChat(`/me @${user.username} Musa do Plug e Rainha da Move It acaba de entrar na sala. :crown:`);
 
-	if (user.id == "1380997")
-		API.sendChat(`/me Preparem-se, o martelo do BAN  @${user.username} acabou de entrar!.`);
-
 	if (user.id == "6511093")
 		API.sendChat(`/me A mulher do xado @${user.username} acaba de entrar na sala. 🦉`);
 
