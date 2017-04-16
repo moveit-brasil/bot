be05ee9db8adbf2dd20b536ee0e3725c840ef18a
Shadow-Slayer <wander.p.c.07@gmail.com>
2017-04-16 19:28:59 -0300

    Removido whatsapp
    Whatsapp: https://chat.whatsapp.com/6EyjDJAeZhX7WDyUqBonlp ?

--- bot.js
+++ bot.js
@@ -3612,7 +3612,7 @@
                                       if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                                       if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                                       else {						
-			API.sendChat('/me Discord: https://discord.me/move-it ? Facebook: https://www.fb.com/groups/MoveItBrasil ? Whatsapp: https://chat.whatsapp.com/6EyjDJAeZhX7WDyUqBonlp ?');
+			API.sendChat('/me Discord: https://discord.me/move-it ? Facebook: https://www.fb.com/groups/MoveItBrasil ?');
                                          }
 			            }
                             },
