3c1ee0f0474a0a9ff08a7d66ecdc6e7db0cda8cf
Shadow-Slayer <wander.p.c.07@gmail.com>
2017-05-01 19:29:07 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1720,8 +1720,8 @@
 					var msg = chat.message;
 					var now = Date.now();
 					var chatters = 0;
-					var time;					
-                        		var timeString = basicBot.roomUtilities.msToStr(time);
+					var time;
+					var tempo = Date.now() - join;                        		
 
 					var launchT = basicBot.room.roomstats.launchTime;
 					var durationOnline = Date.now() - launchT;
@@ -1738,7 +1738,7 @@
 						chatters++;
 					    }
 					}
-					API.sendChat(subChat(basicBot.chat.activeusersintime, {name: chat.un, amount: chatters, time: timeString}));
+					API.sendChat(subChat(basicBot.chat.activeusersintime, {name: chat.un, amount: chatters, time: tempo}));
 				    }
 				}
 			    },
