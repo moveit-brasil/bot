2f3a8b53e3e624f3fcc69e72315c52aa41c294fd
Shadow-Slayer <wander.p.c.07@gmail.com>
2017-05-02 08:27:25 -0300

    Update entrada.js

--- entrada.js
+++ entrada.js
@@ -8,8 +8,8 @@ API.on(API.USER_JOIN, user => {
 	if (user.id == "6511093")
 		API.sendChat(`/me A mulher do xado @${user.username} acaba de entrar na sala. 🦉`);
 
-	if (user.id == "18951868")
-		API.sendChat(`/me Quem for diabético saia, porque o docinho @${user.username} chegou :yum: `);
+	//if (user.id == "18951868")
+	//	API.sendChat(`/me Quem for diabético saia, porque o docinho @${user.username} chegou :yum: `);
 	
 	if (user.id == "5765109")
 		API.sendChat(`/me O mais fofo da sala @${user.username} acaba de entrar. :crown:`); 
