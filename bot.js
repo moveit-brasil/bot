aa593de666048d82420851e483ca1b0d0a3efe20
Shadow-Slayer <wander.p.c.07@gmail.com>
2017-06-12 21:19:21 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3441,8 +3441,7 @@
 				"/me Roleta a cada 30 minutos, fique atento as roletas, uma move para o 2º lugar e a outra para uma posição aleatória!",				
 				"/me Digite !autowoot e instale o RCS para poder ver a customização da sala, foto no chat, etc http://i.imgur.com/X5MGCGi.png ",								
 				"/me Confira nossas regras em - https://moveitbrasil.com.br/regras/",
-				"/me Quer ter uma foto no chat? http://i.imgur.com/5Gyrwby.jpg Digite !foto cadastre-se em nosso site e siga as instruções!",
-				"/me Evento do Dia Dos Namorados, dia 12 às 15h http://i.imgur.com/wpNzl4C.png"];
+				"/me Quer ter uma foto no chat? http://i.imgur.com/5Gyrwby.jpg Digite !foto cadastre-se em nosso site e siga as instruções!"];
 					  c = Math.floor(Math.random() * mensagens.length);
 			  return API.sendChat(mensagens[c]);
 					 }
