fdacb19a3bdbbf80b927b2cd3675869d0cc31639
Shadow-Slayer <wander.p.c.07@gmail.com>
2017-06-25 17:29:17 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3441,6 +3441,7 @@
 				"/me Roleta a cada 30 minutos, fique atento as roletas, uma move para o 2º lugar e a outra para uma posição aleatória!",				
 				"/me Digite !autowoot e instale o RCS para poder ver a customização da sala, foto no chat, etc http://i.imgur.com/X5MGCGi.png ",								
 				"/me Confira nossas regras em - https://moveitbrasil.com.br/regras/",
+				"/me http://i.imgur.com/hkjOsS8.gif",
 				"/me Quer ter uma foto no chat? http://i.imgur.com/5Gyrwby.jpg Digite !foto cadastre-se em nosso site e siga as instruções!"];
 					  c = Math.floor(Math.random() * mensagens.length);
 			  return API.sendChat(mensagens[c]);
